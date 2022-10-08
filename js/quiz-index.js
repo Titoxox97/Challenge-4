f130e69cf23c23b39b372878c7f664ba8eec2e91
matias <matiasahrensdorf@gmail.com>
2022-10-08 18:26:06 -0400

    organize javascript code to be more orderly and readable. Seperate js code into two files: questions/quiz-index will add a third for scores when the time comes

--- js/quiz-index.js
+++ js/quiz-index.js
@@ -1,13 +1,49 @@
 // Variables tracking current state of quiz
 var currentQuestIndex = 0;
 var timeLeft = quizQuestions.length * 10;
-var timer;
+var timerId;
 
 // Variables referencing DOM elements
-var quizQuestions = document.getElementById('questions');
+var quizQuestions = document.getElementById("questions");
 var quizTimer = document.getElementById("time");
 var quizChoices = document.getElementById("choices");
 var submitScoreBtn = document.getElementById("submitScore");
 var startQuizBtn = document.getElementById("startbtn");
 var userInitials = document.getElementById("initials");
 var quizFeedback = document.getElementById("feedback");
+
+function startQuiz() {
+   // hide opening page
+    openpage.style.display = "none";
+
+    // unhide questions
+    quizQuestions.style.display = "block";
+
+    //Begin timer
+    timerId = setInterval(function() {
+        // Count down
+        timeLeft--;
+        // Display time remaining
+        quizTimer.textContent = "Time left: " + timeLeft;
+
+        getQuestion();
+    })
+
+function getQuestion() {
+    // Retrieve current question from question array
+    var currentQuest = quizQuestions[currentQuestIndex];
+    
+    //update question title with current question 
+    var currentTitle = document.getElementById('question-title');
+    currentTitle.textContent = currentQuest
+
+    // Clear previous question choices
+    quizChoices.innerHTML = '';
+
+    // For loop choosing the choice elements
+    for (var i=0; i < currentQuest.choices.length; i++) {
+
+    }
+
+}
+}
