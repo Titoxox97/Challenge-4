ce5065dc833b534fd6157b60eca3eeb2e9e0cd5d
matias <matiasahrensdorf@gmail.com>
2022-10-12 00:05:17 -0400

    First movie question

--- js/questions.js
+++ js/questions.js
@@ -1,51 +1,59 @@
 // List of questions, choices, and the answers
-var quizQuestions = [{
-    question: "JavaScript file has an extension of:",
+var quizQuestions = [
+    {
+    question: "How does Joy help Jack escape from the shed in <q>Room</q>?",
     choiceA: ".Java",
     choiceB: ".javascript",
     choiceC: ".js",
     choiceD: "jscript",
-    correctAnswer: "c"},
+    correctAnswer: "c"
+    },
     {
     question: "If this button is clicked, the event handler is invoked:",
     choiceA: "OnSubmit()",
     choiceB: ".OnLoad()",
     choiceC: "IsPostBack()",
     choiceD: "Onclick()",
-    correctAnswer: "d"},
+    correctAnswer: "d"
+    },
     {
     question: "A function associated with an object is called a:",
     choiceA: "Function",
     choiceB: "API",
     choiceC: "ID",
     choiceD: "Method",
-    correctAnswer: "d"},
+    correctAnswer: "d" 
+    },
     {
     question: "Which built-in method combines the text of two strings and returns a new string?",
     choiceA: "append()",
     choiceB: "concat()",
     choiceC: "attach()",
     choiceD: "section()",
-    correctAnswer: "b"},   
+    correctAnswer: "b"
+    },   
     {
     question: "In JavaScript, what is a block of statement?",
     choiceA: "block containing a single statement",
     choiceB: "a conditional block and a single statement",
     choiceC: "block combining a number of statements into one compound statement",
     choiceD: "A conditional block",
-    correctAnswer: "c"},
+    correctAnswer: "c"
+    },
     {
     question: "The 'function' and 'var' are known as:",
     choiceA: "Keywords",
     choiceB: "Data Types",
     choiceC: "Headers",
     choiceD: "Declaration Statements",
-    correctAnswer: "d"},
+    correctAnswer: "d"
+    },
     {
     question: "In JS what will be used for calling the function definition expression:",
     choiceA: "Function prototype",
     choiceB: "Function literal",
     choiceC: "Function calling",
     choiceD: "Function declaration",
-    correctAnswer: "b"},
+    correctAnswer: "b"
+    },
 ];
