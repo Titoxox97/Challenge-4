85ab73ba804a33beef9ad3f89d55442ac012f483
matias <matiasahrensdorf@gmail.com>
2022-11-05 00:24:44 -0400

    css/html clean up

--- js/logic.js
+++ js/logic.js
@@ -4,7 +4,7 @@ var time = questions.length * 10;
 var timerId;
 
 // variables to reference DOM elements
-var questionsEl = document.getElementById('questions');
+var questionsEl = document.getElementById('qscontainer');
 var timerEl = document.getElementById('time');
 var choicesEl = document.getElementById('choices');
 var submitBtn = document.getElementById('submit');
@@ -15,11 +15,11 @@ var feedbackEl = document.getElementById('feedback');
 
 function startQuiz() {
   // hide start screen
-  var startScreenEl = document.getElementById('start-screen');
+  var startScreenEl = document.getElementById('startcontainer');
   startScreenEl.setAttribute('class', 'hide');
 
   // un-hide questions section
-  questionsEl.removeAttribute('class');
+  questionsEl.removeAttribute('class', 'hide');
 
   // start timer
   timerId = setInterval(clockTick, 1000);
